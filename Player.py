e7620b1b67e3fb292645536b20439ca7a9a647aa
Tadaboody <tomerpet@gmail.com>
2018-04-16 19:00:04 +0300

    Pass player tests
    Always green~!

--- Player.py
+++ Player.py
@@ -1,27 +1,73 @@
 from Card import Card
 import random
+
+
 class Player:
     DEFAULT_HAND_SIZE = 4
 
     def __init__(self, player_num: int):
-        self.play_num = player_num
+        self.player_num = player_num
         self.hand = list()
         for _ in range(Player.DEFAULT_HAND_SIZE - 1):
             self.hand.append(Card(isSkull=False))
         self.hand.append(Card(isSkull=True))
         self.played_cards = list()
-        
+
     def has_skull(self):
         return any(card.isSkull for card in self.hand)
-    
-    def player_input(self,msg_string):
-        return input(msg_string)
 
-    def play_card(self,isRandom=False):
-        if isRandom:
-            played_card = random.choice(self.hand)
-        else:
-            played_card = self.hand[self.player_input('Played card:')] #TODO: export strings
+    def choose_card(self) -> int:
+        return self.legal_input(range(0, len(self.hand)), 'Play card:', int)
+
+    def decide_to_bet(self) -> bool:
+        return self.legal_input("Do you want to bet? (Yes/No)", ['y', 'yes', 'Yes', 'n', 'No'], bool)
+
+    def play_card(self):
+        played_card = self.hand[self.choose_card()]  # TODO: export strings
         self.hand.remove(played_card)
         self.played_cards.append(played_card)
         return played_card
+
+    def play(self, played_cards: int, is_random=False) -> int:
+        if len(self.hand) == 0 or (is_random and bool(random.getrandbits(1))) or self.decide_to_bet():
+            return self.bet(played_cards)
+        else:
+            self.play_card()
+            return 0
+
+    def bet(self, played_cards: int) -> int:
+        return self.legal_input(range(1, played_cards), 'Choose bet', int)
+
+    def discard(self):
+        removed_index = self.choose_card()
+        self.hand.pop(removed_index)
+
+    def pick_reveal(self, num_of_players: int):
+        while self.played_cards:
+            return self.player_num
+        return self.choose_player(num_of_players)
+
+    def choose_player(self, num_of_players) -> int:
+        legal_players = list(range(1, num_of_players))
+        legal_players.remove(self.player_num)
+        return self.legal_input(legal_players, 'Choose other player', int)
+
+    def reset_round(self):
+        while self.played_cards:
+            self.hand.append(self.played_cards.pop())
+
+    def legal_input(self, seq, msg, return_type):
+        legal = False
+        re = None
+        while not legal:
+            re = input(msg)
+            legal = re in seq
+        return return_type(re)
+
+    def raise_bet(self, current_bet: int, played_cards: int):
+        return self.legal_input(list(range(current_bet+1, played_cards)) + [0], 'Raise from 2?', int)
+
+
+class RandomPlayer(Player):
+    def legal_input(self, seq, msg, return_type):
+        return return_type(random.choice(seq))
--- Player_test.py
+++ Player_test.py
@@ -1,19 +1,19 @@
-import unittest
-from Player import Player
+from unittest import TestCase
+from Player import Player, RandomPlayer
 
 DEFAULT_HAND_SIZE = Player.DEFAULT_HAND_SIZE
 
 
-class PlayerTest(unittest.TestCase):
+class PlayerTest(TestCase):
     MOCK_PLAYER_NUM = 1
-    MOCK_BOARD_SIZE = 3  # Currently played cards
-    MOCK_PLAYER_COUNT = 4 
+    MOCK_PLAYED_CARDS = 5  # Currently played cards
+    MOCK_PLAYER_COUNT = 4
 
     def setUp(self):
-        self.player = Player(player_num=PlayerTest.MOCK_PLAYER_NUM)
+        self.player = RandomPlayer(player_num=PlayerTest.MOCK_PLAYER_NUM)
 
     def test_player_num(self):
-        self.player.player_num = PlayerTest.MOCK_PLAYER_NUM
+        self.assertEqual(self.player.player_num, PlayerTest.MOCK_PLAYER_NUM)
 
     def test_starting_hand(self):
         self.assertEqual(len(self.player.hand), DEFAULT_HAND_SIZE)
@@ -21,66 +21,64 @@ class PlayerTest(unittest.TestCase):
 
         def count_skulls(player):
             return len([card for card in self.player.hand if card.isSkull])
+
         self.assertEqual(count_skulls(self.player), 1,
                          msg="Player has more than one skull")
         self.assertEqual(len(self.player.played_cards), 0)
 
     def test_card_play(self):
-        played_card = self.player.play_card(isRandom=True)
+        played_card = self.player.play_card()
         self.assertEqual(len(self.player.hand), DEFAULT_HAND_SIZE - 1)
         self.assertEqual(len(self.player.played_cards), 1)
         self.assertEqual(self.player.played_cards[0], played_card)
 
     def test_play_all_cards(self):
         for _ in range(DEFAULT_HAND_SIZE):
-            self.player.play_card(isRandom=True)
-        self.assertRaises(IndexError, self.player.play_card, isRandom=True)
+            self.player.play_card()
+        self.assertRaises(IndexError, self.player.play_card)
         self.assertEqual(len(self.player.played_cards), DEFAULT_HAND_SIZE)
         self.assertTrue(any(card.isSkull for card in self.player.played_cards))
 
     def test_bets(self):
-        bet = self.player.play(
-            played_cards=PlayerTest.MOCK_BOARD_SIZE, always_bet=True)
+        bet = self.player.bet(played_cards=PlayerTest.MOCK_PLAYED_CARDS)
         self.assertTrue(bet > 0, """player didn't bet""")  # Did bet
-        self.assertTrue(bet <= PlayerTest.MOCK_BOARD_SIZE,
+        self.assertTrue(bet <= PlayerTest.MOCK_PLAYED_CARDS,
                         "Player bet more than legal")
-        self.assertEqual(len(self.player.played_cards),
+        self.assertEqual(len(self.player.hand),
                          DEFAULT_HAND_SIZE, 'betting player played ')
 
     def test_raise(self):
+        test_range = range(10)
         mock_already_bet = 2
-        player_raise = self.player.raise_bet(
-            current_bet=mock_already_bet, always_raise=True)
-        self.assertTrue(player_raise > mock_already_bet)
+        for _ in range(10):
+            player_raise = self.player.raise_bet(
+                current_bet=mock_already_bet, played_cards=PlayerTest.MOCK_PLAYED_CARDS)
+            self.assertIn(player_raise, list(range(mock_already_bet + 1, PlayerTest.MOCK_PLAYED_CARDS+1)) + [0])
 
     def test_corner_bets(self):
         for _ in range(DEFAULT_HAND_SIZE):
-            bet = self.player.play_card(isRandom=True)
-        bet_samples = (self.player.play(isRandom=True)
+            self.player.play_card()
+        bet_samples = (self.player.play(is_random=True, played_cards=PlayerTest.MOCK_PLAYED_CARDS)
                        for _ in range(DEFAULT_HAND_SIZE))  # Sampling for isRandom
         self.assertTrue(all(bet > 0 for bet in bet_samples),
                         'Handless player did not bet')
 
     def test_open_self(self):
-        self.player.play_card(isRandom=True)
-        player_opened = self.player.pick_reveal(isRandom=True)
+        self.player.play_card()
+        player_opened = self.player.pick_reveal(num_of_players=PlayerTest.MOCK_PLAYER_COUNT)
         self.assertEqual(player_opened, self.player.player_num)
 
     def test_open_others(self):
-        player_opened = self.player.pick_reveal(isRandom=True)
+        player_opened = self.player.pick_reveal(num_of_players=PlayerTest.MOCK_PLAYER_COUNT)
         self.assertTrue(
-            player_opened != self.player_num and player_opened <= PlayerTest.MOCK_PLAYER_COUNT)
-    
+            player_opened != self.player.player_num and player_opened <= PlayerTest.MOCK_PLAYER_COUNT)
+
     def test_discard(self):
-        self.player.discard(isRandom=True)
-        self.assertEqual(len(self.player.hand),DEFAULT_HAND_SIZE - 1)
-    
+        self.player.discard()
+        self.assertEqual(len(self.player.hand), DEFAULT_HAND_SIZE - 1)
+
     def test_reset(self):
         for _ in range(DEFAULT_HAND_SIZE):
-            self.player.play_card(isRandom=True)
+            self.player.play_card()
         self.player.reset_round()
         self.test_starting_hand()
-
-        
-
-        
