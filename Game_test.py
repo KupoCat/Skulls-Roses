c2841b88cc1b2602133d7b283e82208ed6ed9bff
Tadaboody <tomerpet@gmail.com>
2018-04-17 13:56:42 +0300

    Add further tests to game
    Not sure when MagickMock actually calls the method...

--- Game_test.py
+++ Game_test.py
@@ -1,6 +1,7 @@
-from .Game import Game
+from Game import Game
 from unittest import TestCase
-
+from unittest.mock import MagicMock,call
+from Card import Card
 MOCK_GAME_SIZE = 4
 MOCK_PLAYED_CARDS = 5
 
@@ -8,13 +9,49 @@ MOCK_PLAYED_CARDS = 5
 class GameTest(TestCase):
     def setUp(self):
         self.game = Game(number_of_players=MOCK_GAME_SIZE, random_players=True)
+        self.game.start_round()
 
     def test_init(self):
         self.assertEqual(self.game.number_of_players, MOCK_GAME_SIZE)
+        
+    def test_start_round(self):
+        self.assertListEqual([len(player.played_cards) for player in self.game.players], [1]*MOCK_GAME_SIZE)
+
+    def test_turn(self):
+        tested_player = self.game.get_player(1)
+        tested_player.play = MagicMock()
+        self.game.turn()
+        tested_player.play.assert_called()
+        self.assertEqual(self.game.current_player, self.game.get_player(2))
+    
+    def test_betting_round(self):
+        tested_player = self.game.get_player(1)
+        tested_player.play = MagicMock(return_value=2)
+        self.game.raise_round = MagicMock()
+        self.game.raise_round.assert_called_with(highest_bet=2, leading_player=tested_player)
+    
+    def test_passing_raise_round(self):
+        tested_player = self.game.get_player(1)
+        passing_players = [player for player in self.game.players if player is not tested_player]
+        for player in passing_players:
+            player.raise_bet = MagicMock(return_value=0)
+        self.game.raise_round(highest_bet=2,leading_player=tested_player)
+        for player in passing_players:
+            player.raise_bet.assert_called_with(current_bet=2)
+    
+    def test_raising_raise_round(self):
+        starting_player = self.game.get_player(1)
+        starting_bet = 1
+        raising_player = self.game.get_player(2)
+        raising_player.raise_bet = MagicMock(return_value=starting_bet+1)
+        self.game.raise_round = MagicMock()
+        self.game.raise_round(highest_bet=starting_bet,leading_player=starting_player)
+        self.game.raise_round.assert_called_with(highest_bet=starting_bet+1, lead_player=raising_player)
 
     def test_round(self):
-        play = self.game.players[0].play()
-        self.assertEqual(play, 0)
+        self.game.turn = MagicMock()
+        self.game.round()
+        self.game.turn.assert_has_calls([call()]*4)
 
     def test_win_points(self):
         winner = self.game.get_winner()
@@ -30,9 +67,27 @@ class GameTest(TestCase):
         self.assertEqual(len(self.game.players), MOCK_GAME_SIZE - 1)
         self.assertEqual(self.game.number_of_players, MOCK_GAME_SIZE - 1)
 
-    def test_successful_choice(self):
-        ...
+    def test_reveal_rose(self):
+        self.game.get_player(2).played_cards = MagicMock(return_value=[Card(isSkull=False)])
+        revealing_player = self.game.get_player(1)
+        revealing_player.choose_player = MagicMock(return_value=2)
+        revealing_player.award_point = MagicMock()
+        self.game.reset_round = MagicMock()
+        self.game.reveal_round(revealing_player=revealing_player,bet=1)
+        revealing_player.award_point.assert_called()
+        self.game.reset_round.assert_called()
 
-    def test_bet_failure(self):
-        ...
-    
+    def test_reveal_skull(self):
+        self.game.get_player(2).played_cards = MagicMock(return_value=[Card(isSkull=True)])
+        revealing_player = self.game.get_player(1)
+        revealing_player.choose_player = MagicMock(return_value=2)
+        revealing_player.remove_card = MagicMock()
+        self.game.reveal_round(revealing_player=revealing_player,bet=1)
+        revealing_player.remove_card.assert_called()
+
+    def test_reset_round(self):
+        for player in self.game.players:
+            player.reset_round = MagicMock()
+        self.game.reset_round()
+        for player in self.game.players:
+            player.reset_round.assert_called()
